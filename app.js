5637335c58e8661aa57d61856aa38fb549d458e1
Aurwa <zafaraurwa11@gmail.com>
2024-06-02 16:26:00 +0500

    Tic_Tac_Toe Project

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const boxes = document.querySelectorAll(".box");
-console.log(boxes);
+//console.log(boxes);
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true; // playerX, player0
+let buttonClicks = 0;
 
 // 2D Array
 const winPatterns = [
@@ -21,31 +25,75 @@ boxes.forEach((box) => {
     if (turnO) {
       box.innerText = "O";
       turnO = false;
+      buttonClicks++;
     } else {
       box.innerText = "X";
+      box.style.color = "#c75bc7";
       turnO = true;
+      buttonClicks++;
     }
-    //box.setAttribute("disabled", "");
     box.disabled = true;
     checkWinner();
   });
 });
 
+// resetGame Function
+const resetGame = () => {
+  buttonClicks = 0;
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+// showWinner Function
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations. Winner is ${winner}!! 🎉`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+// drawMesg Function
+const drawMesg = () => {
+  msg.innerText = `Game Draw. 🙄`;
+  msgContainer.classList.remove("hide");
+};
+
+// disableBoxes Function
+const disableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = true;
+  });
+};
+
+// enableBoxes Function
+const enableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = false;
+    box.innerText = "";
+  });
+};
+
+// checkWinner Function
 const checkWinner = () => {
   for (let pattern of winPatterns) {
-    console.log(pattern[0], pattern[1], pattern[2]);
-
-    console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
-    //   let pos1Val = boxes[pattern[0]].innerText;
-    //   let pos2Val = boxes[pattern[1]].innerText;
-    //   let pos3Val = boxes[pattern[2]].innerText;
-    //   if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-    //     if (pos1Val === pos2Val && pos2Val === pos3Val) {
-    //       console.log("winner");
-    //     }
-    //   }
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log(buttonClicks);
+        // console.log("winner", pos1Val);
+        showWinner(pos1Val);
+      } else if (buttonClicks === 9) {
+        // console.log(buttonClicks);
+        drawMesg();
+      }
+    }
   }
 };
+
+resetBtn.addEventListener("click", resetGame);
+newGameBtn.addEventListener("click", resetGame);
 
 //console.log(pattern[0], pattern[1], pattern[2]);
 
